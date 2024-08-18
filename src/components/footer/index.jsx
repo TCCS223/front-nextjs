42a5008b54324de3047ecf13043211443a2b42be
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-18 00:32:09 -0300

    inclusão do icone de menu no header
    alteração das divs, classes e estilização css do header
    
    componentização do header e do footer (dentro da pasta components)
    
    craição de media query de 500px para visualizar o icone de menu

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,70 @@
+
+import styles from './index.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Footer() {
+    return (
+        <footer>
+            <div className={styles.footerContainer}>
+                <div className={styles.footerLogoSocial}>
+                    <div className={styles.footerLogo}>Urban</div>
+                    <div className={styles.footerSocialIcons}>
+                        <span className={`${styles.socialIcon} ${styles.facebook}`}></span>
+                        <span className={`${styles.socialIcon} ${styles.instagram}`}></span>
+                        <span className={`${styles.socialIcon} ${styles.whatsapp}`}></span>
+                    </div>
+                </div>
+
+                <ul className={styles.footerLinks}>
+                    <h2 className={styles.footerHeading}>Links</h2>
+                    <li>
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span className={styles.icon}></span>Início
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/telas/servicos" className={styles.footerLink}>
+                            <span className={styles.icon}></span>Serviços
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#GALERIA" className={styles.footerLink}>
+                            <span className={styles.icon}></span>Galeria
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#SOBRENOS" className={styles.footerLink}>
+                            <span className={styles.icon}></span>Sobre Nós
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#CONTATO" className={styles.footerLink}>
+                            <span className={styles.icon}></span>Contato
+                        </Link>
+                    </li>
+                </ul>
+
+                <ul className={styles.footerContact}>
+                    <h2 className={styles.footerHeading}>Contato</h2>
+                    <li>
+                        <h3 className={styles.contactTitle}><span className={styles.icon}></span>Telefone</h3>
+                        <p className={styles.contactDetail}>(99) 9999-9999</p>
+                    </li>
+                    <li>
+                        <h3 className={styles.contactTitle}><span className={styles.icon}></span>Email</h3>
+                        <p className={styles.contactDetail}>urban@gmail.com</p>
+                    </li>
+                    <li>
+                        <h3 className={styles.contactTitle}><span className={styles.icon}></span>Endereço</h3>
+                        <p className={styles.contactDetail}>Av. São Cristovão, 22 / Tupã-SP</p>
+                    </li>
+                </ul>
+            </div>
+            <div className={styles.footerBottom}>
+                <p>Urban © 2024 / Todos os direitos reservados.</p>
+                <Link href="#" className={styles.footerTerms}>Termos & Políticas</Link>
+            </div>
+        </footer>
+    )
+}
---
+++ src/components/header/index.jsx
@@ -0,0 +1,109 @@
+import styles from './index.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+            <nav className={styles.containerNav}>
+                <div className={styles.menu}>
+                    <div className={styles.logo}>Logo</div>
+                    <div className={styles.navbar}>
+                        <ul className={styles.navlist}>
+                            <li><Link href="#" className={styles.linkNav}>Home</Link></li>
+                            <li><Link href="#" className={styles.linkNav}>Sobre</Link></li>
+                            <li><Link href="#" className={styles.linkNav}>Contato</Link></li>
+                            <li><Link href="/app/telas/cadastro" className={styles.linkNav}>Cadastro</Link></li>
+                            <li><Link href="/app/telas/login" className={styles.linkNav}>Login</Link></li>
+                        </ul>
+                        <div className={styles.menuMobile}>
+                            <Image
+                                src={'/icons/menuMobile.svg'}
+                                width={32}
+                                height={32}
+                                alt='icon menu'
+                            ></Image>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+
+
+
+
+/*<li className={styles.dropdown}>
+                        <Image
+                            src={'menuham.png'}
+                            alt={"Menu"}
+                            width={24}
+                            height={24}
+                            unoptimized={true}
+                            className={styles.menuIcon}
+                        />
+                        <ul className={styles.dropdownmenu}>
+                            <li><a href="#">Desenvolvimento Web</a></li>
+                            <li><a href="#">Design Gráfico</a></li>
+                            <li><a href="#">Marketing Digital</a></li>
+                        </ul>
+                    </li>
+                    
+                    
+                    .dropdown {
+  position: relative;
+  display: flex;
+  align-items: center;
+
+}
+
+.menuIcon {
+  cursor: pointer;
+  display: inline-block;
+  width: 1.5rem;
+
+  height: 1.7rem;
+
+}
+
+.dropdownmenu {
+  display: none;
+  position: absolute;
+  top: 100%;
+  left: 0;
+  background-color: #f8f9fa;
+  padding: 10px 0;
+  list-style: none;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.dropdownmenu a {
+  display: block;
+  padding: 10px 15px;
+  color: #333;
+}
+
+.dropdownmenu a:hover {
+  background-color: #e9ecef;
+}
+
+.dropdown:hover .dropdownmenu {
+  display: block;
+}
+
+@media (max-width: 768px) {
+  .navlist {
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .dropdownmenu {
+    position: static;
+    box-shadow: none;
+    z-index: 7;
+  }
+}
+                    */
