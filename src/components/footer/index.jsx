08477953b51a887d69bd891ecededd40320ef1f9
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-21 22:50:50 -0300

    commit footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,104 +1,102 @@
-
 import styles from './index.module.css'
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Footer() {
     return (
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
                 <div className={styles.footerIcons}>
+
                     <span className={styles.footerLogo}></span>
+
                     <div className={styles.footerSocialIcons}>
-                            <Link href={'https://instagram.com'}>
-                                <span className={styles.iconInstagram}></span>
-                            </Link>
-                            <Link href={'https://whatsapp.com'}>
-                                <span className={styles.iconWhatsapp}></span>
-                            </Link>
-                            <Link href={'https://facebook.com'}>
-                                <span className={styles.iconFacebook}></span>
-                            </Link>
-                            <Link href={'https://youtube.com'}>
-                                <span className={styles.iconYoutube}></span>
-                            </Link>
-                        </div>
-                </div>
-                <div className={styles.footerLinks}>
-                <h2 className={styles.footerHeading}>Links de navegação</h2>
-            
-            <div className={styles.footerLinksNavigation}>
-                        <Link href="#HOME" className={styles.footerLink}>
-                        <span class={styles.arrowLink}></span>Início
+                        <Link href={'https://instagram.com'}>
+                            <span className={styles.iconInstagram}></span>
                         </Link>
-
-                     
-                        <Link href="#HOME" className={styles.footerLink}>
-                        <span class={styles.arrowLink}></span>Serviços
+                        <Link href={'https://whatsapp.com'}>
+                            <span className={styles.iconWhatsapp}></span>
                         </Link>
-
-                        <Link href="#HOME" className={styles.footerLink}>
-                        <span class={styles.arrowLink}></span>Sobre Nós
+                        <Link href={'https://facebook.com'}>
+                            <span className={styles.iconFacebook}></span>
                         </Link>
-
-                        <Link href="#HOME" className={styles.footerLink}>
-                        <span class={styles.arrowLink}></span>Contato
+                        <Link href={'https://youtube.com'}>
+                            <span className={styles.iconYoutube}></span>
                         </Link>
-                  
-                        </div>
-                </div>
-                <div className={styles.footerAddress}></div>
-                <div className={styles.footerLicense}></div>
-               
-     
-
-
-            </div>
-
-
-
-
-
-
-            {/* <div className={styles.footerContainer}>
-                <div className={styles.footerLogoSocial}>
-                    <div className={styles.footerLogo}>Urban</div>
-                    <div className={styles.footerSocialIcons}>
-                        <span className={`${styles.socialIcon} ${styles.facebook}`}></span>
-                        <span className={`${styles.socialIcon} ${styles.instagram}`}></span>
-                        <span className={`${styles.socialIcon} ${styles.whatsapp}`}></span>
                     </div>
                 </div>
 
-                <ul className={styles.footerLinks}>
-                    <h2 className={styles.footerHeading}>Links</h2>
-                    <li>
-                        <Link href="#HOME" className={styles.footerLink}>
-                            <span className={styles.icon}></span>Início
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/telas/servicos" className={styles.footerLink}>
-                            <span className={styles.icon}></span>Serviços
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#GALERIA" className={styles.footerLink}>
-                            <span className={styles.icon}></span>Galeria
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#SOBRENOS" className={styles.footerLink}>
-                            <span className={styles.icon}></span>Sobre Nós
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="#CONTATO" className={styles.footerLink}>
-                            <span className={styles.icon}></span>Contato
-                        </Link>
-                    </li>
-                </ul>
+                <div className={styles.footerLinks}>
 
+                    <h2 className={styles.footerHeading}>Links de navegação</h2>
+
+                    <div className={styles.footerLinksNavigation}>
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span class={styles.arrowLink}></span>Início
+                        </Link>
+
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span class={styles.arrowLink}></span>Serviços
+                        </Link>
+
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span class={styles.arrowLink}></span>Sobre Nós
+                        </Link>
+
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span class={styles.arrowLink}></span>Contato
+                        </Link>
+
+                        <Link href="#HOME" className={styles.footerLink}>
+                            <span class={styles.arrowLink}></span>Login
+                        </Link>
+                    </div>
+                </div>
+
+                <div className={styles.footerAddress}>
+
+                    <h2 className={styles.footerHeading}>Contato</h2>
+
+                    <div className={styles.informations}>
+                        <div className={styles.phone}>
+                            <span class={styles.iconPhone}></span>
+                            <div className={styles.infoPhone}>
+                                <div className={styles.titlePhone}>Telefone</div>
+                                <div className={styles.numberPhone}>(99) 9999-9999</div>
+                            </div>
+                        </div>
+
+                        <div className={styles.email}>
+                            <span class={styles.iconEmail}></span>
+                            <div className={styles.infoEmail}>
+                                <div className={styles.titleEmail}>Email</div>
+                                <div className={styles.addressEmail}>urban@teste.com</div>
+                            </div>
+                        </div>
+
+                        <div className={styles.address}>
+                            <span class={styles.iconAddress}></span>
+                            <div className={styles.infoAddress}>
+                                <div className={styles.titleAddress}>Endereço</div>
+                                <div className={styles.localizationEmail}>Av. São Cristovão, 22 / Tupã-SP</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.footerLicense}>
+                    <span className={styles.copyright}>Urban © 2024 / Todos os direitos reservados</span>
+                    <Link href="#" className={styles.footerTerms}>Termos & Políticas</Link>
+                </div>
+            </div>
+        </footer >
+    )
+}
+
+
+
+
+
+/*
                 <ul className={styles.footerContact}>
                     <h2 className={styles.footerHeading}>Contato</h2>
                     <li>
@@ -118,7 +116,4 @@ export default function Footer() {
             <div className={styles.footerBottom}>
                 <p>Urban © 2024 / Todos os direitos reservados.</p>
                 <Link href="#" className={styles.footerTerms}>Termos & Políticas</Link>
-            </div> */}
-        </footer>
-    )
-}
+            </div> */
