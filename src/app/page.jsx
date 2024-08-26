7586e11a46cc2607c2b445063853b7f32f2223e8
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-08-26 16:04:05 -0300

    commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -19,9 +19,9 @@ export default function Home() {
                 <main className={styles.main}>
                     {/* Seção 1 */}
                     <section className={styles.section}>
-                        <TypingEffect />
-                            {/* <h2 className={styles.sectionTitle1}>Seu carro na melhor performance!</h2> */}
-                            {/* <p className={styles.sectionContent1}>Revele a essência do seu automóvel</p> */}
+                        {/* <TypingEffect /> */}
+                            <h2 className={styles.sectionTitle1}>Seu carro na melhor performance!</h2>
+                            <p className={styles.sectionContent1}>Revele a essência do seu automóvel</p>
                     </section>
 
                     <div className={styles.divider}></div>
