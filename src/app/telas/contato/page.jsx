ac65e7e5f2a1ebf7687e012cbfe6da5c16f8d87d
Matheus Mota Tonini <138072866+motaxyz@users.noreply.github.com>
2024-08-30 17:44:55 -0300

    commit

--- src/app/telas/contato/page.jsx
+++ src/app/telas/contato/page.jsx
@@ -7,33 +7,37 @@ import Footer from '@/components/footer';
 export default function Contato() {
     return (
         <div>
-            <Header/>
+            <Header />
             <div className={styles.backgroundcontato}>
-            <main className={styles.main}>
-                <section className={styles.section}>
-                    <div className={styles.container_contato}>
-                        <div className={styles.titulocontato}>Fale conosco</div>
-                        <div className={styles.bloco}>
-                            <div className={styles.fontblocotituloinicial}>Dúvidas</div>
-                            {/* <hr className={styles.hr}> */}
-                            <div className={styles.fontblocotxt}>Telefone: (XX)XXXX-XXXX</div>
-                            <div className={styles.fontblocotxt}>WhatsApp: (XX)XXXX-XXXX</div>
-                            <div className={styles.fontblocotxt}>Email: urban_ouvidoria@teste.com</div>
+                <main className={styles.main}>
+                    <section className={styles.section}>
+                        <div className={styles.container_contato}>
+                            <div className={styles.titulocontato}>Fale conosco</div>
+                            <div className={styles.bloco}>
+                                <div className={styles.fontblocotituloinicial}>Dúvidas</div>
+                                {/* <hr className={styles.hr}> */}
+                                <div className={styles.fontblocotxt}>Telefone: (XX)XXXX-XXXX</div>
+                                <div className={styles.fontblocotxt}>WhatsApp: (XX)XXXX-XXXX</div>
+                                <div className={styles.fontblocotxt}>Email: urban_ouvidoria@teste.com</div>
 
-                            <div className={styles.fontblocotitulo}>Reclamações</div>
-                            <div className={styles.fontblocotxt}>Telefone: (XX)XXXX-XXXX</div>
-                            <div className={styles.fontblocotxt}>WhatsApp: (XX)XXXX-XXXX</div>
-                            <div className={styles.fontblocotxt}>Email: urban_atendimento@teste.com</div>
+                                <div className={styles.fontblocotitulo}>Reclamações</div>
+                                <div className={styles.fontblocotxt}>Telefone: (XX)XXXX-XXXX</div>
+                                <div className={styles.fontblocotxt}>WhatsApp: (XX)XXXX-XXXX</div>
+                                <div className={styles.fontblocotxt}>Email: urban_atendimento@teste.com</div>
 
-                            <div className={styles.fontblocotitulo}>Trabalhe conosco</div>
-                            <div className={styles.fontblocotxt}>Email: urban_rh@teste.com</div>
+                                <div className={styles.fontblocotitulo}>Trabalhe conosco</div>
+                                <div className={styles.fontblocotxt}>Email: urban_rh@teste.com</div>
 
-                            <div className={styles.fontblocotitulo}>Seja nosso parceiro</div>
-                            <div className={styles.fontblocotxt}>Email: urban_parcerias@teste.com</div>
+                                <div className={styles.fontblocotitulo}>Seja nosso parceiro</div>
+                                <div className={styles.fontblocotxt}>Email: urban_parcerias@teste.com</div>
+
+                            </div>
+                            <div className={styles.localmapa}>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1100.2840817834017!2d-50.52772093878624!3d-21.93840469647665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9495b7e39ba13817%3A0xf46b63b9addcc184!2sEscola%20T%C3%A9cnica%20Estadual%20Professor%20Massuyuki%20Kawano%20-%20Tup%C3%A3!5e0!3m2!1spt-BR!2sbr!4v1725049409623!5m2!1spt-BR!2sbr" className={styles.maps}></iframe>
+                            </div>
                         </div>
-                    </div>
-                </section>
-            </main>
+                    </section>
+                </main>
             </div>
             <Footer />
         </div>
