67367c4cadfba9b2751c53a478c861bac2ccb98e
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-09-18 22:52:36 -0300

    commit

--- src/app/telas/admin/cadcliente/page.jsx
+++ src/app/telas/admin/cadcliente/page.jsx
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 import { useState, useEffect, useRef } from 'react';
 import React from "react";
 import ConsultaCliente from '@/components/modais/modais_clientes';
+import { format } from 'date-fns';
 
 import api from '@/services/api';
 
@@ -13,6 +14,13 @@ export default function CadCliente() {
     const [isModalOpen, setIsModalOpen] = useState(false);
 
     const selectSexo = useRef(null);
+
+    const sexoMap = {
+        0: 'Feminino',
+        1: 'Masculino',
+        2: 'Outro'
+    };
+
 
     const openModal = () => {
         setIsModalOpen(true);
@@ -87,74 +95,81 @@ export default function CadCliente() {
             <ConsultaCliente isOpen={isModalOpen} onClose={closeModal} />
 
             <form id="clienteForm" className={styles.form}>
-                    {/* <input type="hidden" id="clienteId" className={styles.input_cliente} /> */}
+                    <input type="hidden" id="clienteId" className={styles.input_cliente} />
 
                     <div className={styles.grid}>
-                    <div className={`${styles.grid_item} ${styles.grid_codigo}`}>
-                        <label for="codigo_cliente" className={styles.label_cliente}>Código</label>
-                        <input type="number" id="codigo_cliente" name="codigo_cliente" required className={styles.input_cliente} />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_codigo}`}>
+                            <label for="codigo_cliente" className={styles.label_cliente}>Código</label>
+                            <input type="number" id="codigo_cliente" name="codigo_cliente" required className={styles.input_cliente} />
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_nome}`}>
-                        <label for="nome_cliente" className={styles.label_cliente} >Nome</label>
-                        <input type="text" id="nome_cliente" name="nome_cliente" required className={styles.input_cliente} placeholder="Nome Completo" />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_nome}`}>
+                            <label for="nome_cliente" className={styles.label_cliente} >Nome</label>
+                            <input type="text" id="nome_cliente" name="nome_cliente" required className={styles.input_cliente} placeholder="Nome Completo" />
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_cpf}`}>
-                        <label for="cpf_cliente" className={styles.label_cliente}>CPF</label>
-                        <input type="text" id="cpf_cliente" name="cpf_cliente" required className={styles.input_cliente} placeholder="xxx.xxx.xxx - xx" />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_cpf}`}>
+                            <label for="cpf_cliente" className={styles.label_cliente}>CPF</label>
+                            <input type="text" id="cpf_cliente" name="cpf_cliente" required className={styles.input_cliente} placeholder="xxx.xxx.xxx - xx" />
+                        </div>
 
 
-                    <div className={`${styles.grid_item} ${styles.grid_data}`}>
-                        <label for="data_nasc_cliente" className={styles.label_cliente}>Data de nascimento</label>
-                        <input type="date" id="data_nasc_cliente" name="data_nasc_cliente" required className={styles.input_cliente} />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_data}`}>
+                            <label for="data_nasc_cliente" className={styles.label_cliente}>Data de nascimento</label>
+                            <input type="date" id="data_nasc_cliente" name="data_nasc_cliente" required className={styles.input_cliente} />
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_sexo}`}>
-                        <label for="sexo_cliente" className={styles.label_cliente}>Sexo</label>
-                        <select ref={selectSexo} onClick={handleselectSexo} id="sexo_cliente" name="sexo_cliente" required className={`${styles.select_cliente} ${styles.input_sexo}`}>
-                            <option value="" disabled selected>Selecionar</option>
-                            <option value="0">Masculino</option>
-                            <option value="1">Feminino</option>
-                            <option value="2">Outro</option>
-                        </select>
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_sexo}`}>
+                            <label for="sexo_cliente" className={styles.label_cliente}>Sexo</label>
+                            <select ref={selectSexo} onClick={handleselectSexo} id="sexo_cliente" name="sexo_cliente" required className={`${styles.select_cliente} ${styles.input_sexo}`}>
+                                <option value="" disabled selected>Selecionar</option>
+                                <option value="0">Masculino</option>
+                                <option value="1">Feminino</option>
+                                <option value="2">Outro</option>
+                            </select>
 
-                    <div className={`${styles.grid_item} ${styles.grid_acesso}`}>
-                        <label for="nivel_acesso" className={styles.label_cliente}>Nível de Acesso</label>
-                        <select id="nivel_acesso" name="nivel_acesso"
-                            className={`${styles.select_cliente} ${styles.input_acesso}`}>
-                            <option value="0" className={styles.option}>Usuário</option>
-                            <option value="1" className={styles.option}>Administrador</option>
-                        </select>
-                    </div>
+                            {/* <select ref={selectSexo} onChange={handleselectSexo} id="sexo_cliente" name="sexo_cliente" required className={`${styles.select_cliente} ${styles.input_sexo}`}>
+                                <option value="" disabled>Selecionar</option>
+                                <option value="0">Masculino</option>
+                                <option value="1">Feminino</option>
+                                <option value="2">Outro</option>
+                            </select> */}
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_telefone}`}>
-                        <label for="telefone_cliente" className={styles.label_cliente}>Telefone</label>
-                        <input type="tel" id="telefone_cliente" name="telefone_cliente" required className={` ${styles.input_cliente}`} placeholder="(xx) xxxxx - xxxxx" />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_acesso}`}>
+                            <label for="nivel_acesso" className={styles.label_cliente}>Nível de Acesso</label>
+                            <select id="nivel_acesso" name="nivel_acesso"
+                                className={`${styles.select_cliente} ${styles.input_acesso}`}>
+                                <option value="0" className={styles.option}>Usuário</option>
+                                <option value="1" className={styles.option}>Administrador</option>
+                            </select>
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_email}`}>
-                        <label for="email_cliente" className={styles.label_cliente}>Email</label>
-                        <input type="email_cliente" id="email_cliente" name="email_cliente" required className={styles.input_cliente} placeholder="exemplo@exemplo.com" />
-                    </div>
-                    <div className={`${styles.grid_item}  ${styles.grid_observacoes}`}>
-                        <label htmlFor="observacoes_cliente" className={styles.label_cliente}>Observações</label>
-                        <input type="text" id="observacoes_cliente" name="observacoes_cliente" required className={styles.input_cliente} />
-                    </div>
+                        <div className={`${styles.grid_item} ${styles.grid_telefone}`}>
+                            <label for="telefone_cliente" className={styles.label_cliente}>Telefone</label>
+                            <input type="tel" id="telefone_cliente" name="telefone_cliente" required className={` ${styles.input_cliente}`} placeholder="(xx) xxxxx - xxxxx" />
+                        </div>
 
-                    <div className={`${styles.grid_item} ${styles.grid_situacao}`}>
-                        <label for="situacao_cliente" className={styles.label_cliente}>Situação</label>
-                        <select id="situacao_cliente" name="situacao_cliente"
-                            className={`${styles.select_cliente} ${styles.input_situacao}`}>
-                            <option value="ativo" className={styles.option} selected>Ativo</option>
-                            <option value="inativo" className={styles.option}>Inativo</option>
-                        </select>
-                    </div>
-                </div>
-            </form>
+                        <div className={`${styles.grid_item} ${styles.grid_email}`}>
+                            <label for="email_cliente" className={styles.label_cliente}>Email</label>
+                            <input type="email_cliente" id="email_cliente" name="email_cliente" required className={styles.input_cliente} placeholder="exemplo@exemplo.com" />
+                        </div>
+                        <div className={`${styles.grid_item}  ${styles.grid_observacoes}`}>
+                            <label htmlFor="observacoes_cliente" className={styles.label_cliente}>Observações</label>
+                            <input type="text" id="observacoes_cliente" name="observacoes_cliente" required className={styles.input_cliente} />
+                        </div>
 
+                        <div className={`${styles.grid_item} ${styles.grid_situacao}`}>
+                            <label for="situacao_cliente" className={styles.label_cliente}>Situação</label>
+                            <select id="situacao_cliente" name="situacao_cliente"
+                                className={`${styles.select_cliente} ${styles.input_situacao}`}>
+                                <option value="ativo" className={styles.option} selected>Ativo</option>
+                                <option value="inativo" className={styles.option}>Inativo</option>
+                            </select>
+                        </div>
+                    </div>
+                </form> 
+            {/* 
             <div className={styles.footer_form}>
                 <button type="reset" onClick={Cancelar} className={styles.button_cancel}>Cancelar</button>
                 <button type="submit" className={styles.button_submit}>Salvar</button>
@@ -162,8 +177,45 @@ export default function CadCliente() {
 
             {/* Tabela de Usuários */}
             <div className={styles.user_table}>
-                <h3>Lista de Usuários</h3>
-                <table>
+                <div className={styles.resultTableContainer}>
+                    <table className={styles.resultTable}>
+                        <thead className={styles.tableHead}>
+                            <th className={`${styles.tableHeader} ${styles.id}`}>Código</th>
+                            <th className={`${styles.tableHeader} ${styles.nome}`}>Nome</th>
+                            <th className={`${styles.tableHeader} ${styles.cpf}`}>CPF</th>
+                            <th className={`${styles.tableHeader} ${styles.dataNasc}`}>Data de Nascimento</th>
+                            <th className={`${styles.tableHeader} ${styles.sexo}`}>Sexo</th>
+                            <th className={`${styles.tableHeader} ${styles.telefone}`}>Telefone</th>
+                            <th className={`${styles.tableHeader} ${styles.email}`}>Email</th>
+                            <th className={`${styles.tableHeader} ${styles.observ}`}>Observações</th>
+                            <th className={`${styles.tableHeader} ${styles.acesso}`}>Acesso</th>
+                            <th className={`${styles.tableHeader} ${styles.situacao}`}>Situação</th>
+                        </thead>
+
+                        <tbody className={styles.tableBody}>
+                            {usuarios.length > 0 ? (
+                                usuarios.map((usuario) => (
+                                    <tr key={usuario.usu_id}>
+                                        <td >{usuario.usu_id}</td>
+                                        <td>{usuario.usu_nome}</td>
+                                        <td>{usuario.usu_cpf}</td>
+                                        <td>{format(new Date(usuario.usu_data_nasc), 'dd/MM/yyyy')}</td>
+                                        <td>{sexoMap[usuario.usu_sexo] || 'Desconhecido'}</td>
+                                        <td>{usuario.usu_telefone}</td>
+                                        <td>{usuario.usu_email}</td>
+                                        <td className={styles.observ}>{usuario.usu_observ}</td>
+                                        <td>{usuario.usu_acesso === 0  ? 'Usuário' : 'Administrador'}</td>
+                                        <td>{usuario.usu_situacao === 1 ? 'Ativo' : 'Inativo'}</td>
+                                    </tr>))
+                            ) : (
+                                <tr>
+                                    <td colSpan="5">Nenhum usuário encontrado</td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </div>
+                {/* <table>
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -175,7 +227,7 @@ export default function CadCliente() {
                     </thead>
                     <tbody>
                         {usuarios.length > 0 ? (
-                            usuarios.map((usuario) => (                                                              
+                            usuarios.map((usuario) => (
                                 <tr key={usuario.usu_id}>
                                     <td>{usuario.usu_id}</td>
                                     <td>{usuario.usu_nome}</td>
@@ -190,7 +242,7 @@ export default function CadCliente() {
                             </tr>
                         )}
                     </tbody>
-                </table>
+                </table> */}
             </div>
         </div>
     );
--- src/app/telas/usuario/UsuarioVeiculos/page.jsx
+++ src/app/telas/usuario/UsuarioVeiculos/page.jsx
@@ -1,37 +1,67 @@
-import React from "react";
-import Image from "next/image";
+import React, { useState } from "react";
 import Link from "next/link";
-
+import ConsultaVeiculo from "@/components/modais/modais_veiculos"; // Importa o formulário
 import styles from "./page.module.css";
 
 export default function UsuarioVeiculos() {
-    return (
-        <div className={styles.container}>
+  const [showForm, setShowForm] = useState(false);
+  const [selectedVehicle, setSelectedVehicle] = useState(null);
 
-            <ol className={styles.fundocards}>
+  const handleAlterarClick = (veiculo) => {
+    // Define o veículo selecionado e exibe o formulário
+    setSelectedVehicle(veiculo);
+    setShowForm(true);
+  };
 
-                <li className={styles.lista}>
-                    <div className={styles.icone}><span className={styles.iconeCarro}></span></div>
-
-                    {/* retornar da api */}
-                    <div className={styles.content}>
-                        <span className={styles.placa}>PLA-4884</span>
-                        <span className={styles.marca}>Mercedes</span>
-                        <span className={styles.modelo}>Z-4</span>
-                    </div>
-                </li>
-                {/* <li className={styles.lista}>
-                    <span className={styles.placa}>Placa</span>
-                    <span className={styles.marca}>Marca</span>
-                    <span className={styles.modelo}>Modelo</span>
-                </li> */}
-                {/* <div className={styles.card1}>
-            
-            </div>
-            <div className={styles.card1}>
-            
-            </div> */}
-            </ol>
+  return (
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.botaonovo}>
+          <button id="novoCliente">Novo</button>
         </div>
-    )
+      </div>
+
+      <div className={styles.container}>
+        {!showForm ? (
+          <ol className={styles.fundocards}>
+            <li className={styles.lista}>
+              <div className={styles.icone}>
+                <span className={styles.iconeCarro}></span>
+              </div>
+
+              <div className={styles.botoeslink}>
+                <Link href="/UsuarioVeiculos/alterarVeiculo" className={styles.link}>
+                  <span className={styles.iconeExcluir}></span>
+                </Link>
+                <button
+                  onClick={() =>
+                    handleAlterarClick({
+                      placa: "PLA-4884",
+                      marca: "Mercedes",
+                      modelo: "Z-4",
+                    })
+                  }
+                  className={styles.link}
+                >
+                  <span className={styles.iconeAlterar}></span>
+                </button>
+              </div>
+              <div className={styles.content}>
+                <span className={styles.placa}>PLA-4884</span>
+                <span className={styles.marca}>Mercedes</span>
+                <span className={styles.modelo}>Z-4</span>
+              </div>
+            </li>
+          </ol>
+        ) : (
+          // Exibe o formulário com os dados do veículo selecionado
+          <ConsultaVeiculo
+            isOpen={showForm}
+            onClose={() => setShowForm(false)}
+            vehicle={selectedVehicle}
+          />
+        )}
+      </div>
+    </>
+  );
 }
