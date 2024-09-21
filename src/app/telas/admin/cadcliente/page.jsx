a5d8dba4284b2529d2d3f58982a3bae060072a62
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-09-21 02:52:08 -0300

    FUNÇÃO DE PESQUISA IMPLEMENTADA
    corrigir a combinação de filtro de pesquisa

--- src/app/telas/admin/cadcliente/page.jsx
+++ src/app/telas/admin/cadcliente/page.jsx
@@ -2,34 +2,65 @@
 
 import styles from './page.module.css';
 import Swal from 'sweetalert2';
-import { useState, useEffect, useRef } from 'react';
-import React from "react";
-import ConsultaCliente from '@/components/modais/modais_clientes';
-import FormCliente from '@/components/FormCliente';
+import { useState, useEffect } from 'react';
 import { MdRemoveRedEye, MdEdit } from "react-icons/md";
 import { IoMdTrash } from "react-icons/io";
-
 import { format } from 'date-fns';
-
 import api from '@/services/api';
+import FormCliente from '@/components/FormCliente';
 
 export default function CadCliente() {
-    const selectSexo = useRef(null);
-
-
-    // const [isModalOpen, setIsModalOpen] = useState(false);
     const [usuarios, setUsuarios] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
+    const [statusFilter, setStatusFilter] = useState('todos');
+    const [tipoUsuarioFilter, setTipoUsuarioFilter] = useState('todos');
+    const [searchText, setSearchText] = useState('');
     const [showForm, setShowForm] = useState(false);
     const [selectedUser, setSelectedUser] = useState(null);
     const [isViewing, setIsViewing] = useState(false);
-    const [currentPage, setCurrentPage] = useState(1);
 
-    const usersPerPage = 15; // Número de usuários por página
-    const indexOfLastUser = currentPage * usersPerPage;
-    const indexOfFirstUser = indexOfLastUser - usersPerPage;
-    const currentUsers = usuarios.slice(indexOfFirstUser, indexOfLastUser);
+    useEffect(() => {
+        ListarUsuarios();
+    }, []);
 
+    useEffect(() => {
+        setFilteredUsers(usuarios); // Inicializa com todos os usuários
+    }, [usuarios]);
 
+    const ListarUsuarios = async () => {
+        try {
+            const response = await api.get('/usuarios');
+            setUsuarios(response.data.dados); // Atualiza o estado com os dados dos usuários
+        } catch (error) {
+            console.error("Erro ao buscar os usuários:", error);
+            Swal.fire({
+                title: "Erro!",
+                text: "Não foi possível carregar os usuários.",
+                icon: "error",
+            });
+        }
+    };
+
+    const handleSearch = () => {
+        const result = usuarios.filter(usuario => {
+            const statusMatch = statusFilter === 'todos' || usuario.usu_situacao === statusFilter;
+    
+            const tipoMatch = tipoUsuarioFilter === 'todos' || 
+                              (tipoUsuarioFilter === 'admin' && usuario.usu_acesso === 1) || 
+                              (tipoUsuarioFilter === 'usuario' && usuario.usu_acesso === 0);
+    
+            const textMatch = 
+                usuario.usu_nome.toLowerCase().includes(searchText.toLowerCase()) || 
+                usuario.usu_email.toLowerCase().includes(searchText.toLowerCase()) ||
+                usuario.usu_cpf.includes(searchText); // Adicione mais campos se necessário
+    
+            // Retornar verdadeiro se todas as condições forem atendidas
+            return statusMatch && tipoMatch && textMatch;
+        });
+    
+        setFilteredUsers(result);
+    };
+    
 
 
     const handleViewUser = (usuario) => {
@@ -44,15 +75,9 @@ export default function CadCliente() {
         setIsViewing(false); // Muda para modo de edição
     };
 
-    console.log(selectedUser); // Verifique se todos os campos estão preenchidos corretamente
-
-
-    const handleSubmit = async (event) => {
-        console.log("Dados enviados:", selectedUser);
-        event.preventDefault();
+    const handleSubmit = async (data) => {
         try {
-            const response = await api.patch(`/usuarios/${selectedUser.usu_id}`, selectedUser);
-            console.log(response); // Verifique o que está sendo retornado
+            const response = await api.patch(`/usuarios/${data.usu_id}`, data);
             Swal.fire({
                 title: 'Sucesso!',
                 text: response.data.mensagem,
@@ -70,82 +95,10 @@ export default function CadCliente() {
         }
     };
 
-
-
-    const sexoMap = {
-        0: 'Feminino',
-        1: 'Masculino',
-        2: 'Outro'
-    };
-
-
-    const openModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-
-    const handleselectSexo = () => {
-        const sexo = selectSexo.current.value;
-        console.log(sexo);
-    };
-
-
-
     const Cancelar = () => {
-        Swal.fire({
-            title: "Deseja Cancelar?",
-            text: "As informações não serão salvas",
-            icon: "warning",
-            iconColor: "orange",
-            showCancelButton: true,
-            cancelButtonColor: "#d33",
-            confirmButtonColor: "rgb(40, 167, 69)",
-            cancelButtonText: "Cancelar",
-            confirmButtonText: "Confirmar",
-            reverseButtons: true,
-            backdrop: "rgba(0,0,0,0.7)",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                Swal.fire({
-                    title: "Cancelado!",
-                    text: "As alterações foram canceladas.",
-                    icon: "success",
-                    iconColor: "rgb(40, 167, 69)",
-                    confirmButtonColor: "rgb(40, 167, 69)",
-                }).then(() => {
-                    setShowForm(false);
-                    setSelectedUser(null); // Mostrar a tabela novamente após confirmação
-                });
-            }
-        });
+        setShowForm(false);
+        setSelectedUser(null); // Mostrar a tabela novamente após confirmação
     };
-
-    // Função para buscar os usuários do banco
-    async function ListarUsuarios() {
-        try {
-            const response = await api.get('/usuarios');
-            // console.log(Array.isArray(teste)); // Adicionando um log para inspecionar os dados
-            setUsuarios(response.data.dados); // Atualiza o estado com os dados dos usuários
-        } catch (error) {
-            console.error("Erro ao buscar os usuários:", error.response ? error.response.data : error.message);
-            Swal.fire({
-                title: "Erro!",
-                text: "Não foi possível carregar os usuários.",
-                icon: "error",
-                confirmButtonColor: "rgb(40, 167, 69)",
-            });
-        }
-    }
-
-
-
-
-    useEffect(() => {
-        ListarUsuarios(); // Chama a função quando o componente é montado
-    }, []);
 
     return (
         <div id="clientes" className={styles.content_section}>
@@ -155,15 +108,24 @@ export default function CadCliente() {
                 <>
                     <div className={styles.contentSearch}>
                         <div className={styles.searchBar}>
-
-                            <input type="text" placeholder="Digite aqui..." className={styles.searchInput} />
-                            <button className={styles.searchButton}>Pesquisar</button>
+                            <input
+                                type="text"
+                                placeholder="Digite aqui..."
+                                className={styles.searchInput}
+                                value={searchText}
+                                onChange={(e) => setSearchText(e.target.value)}
+                            />
+                            <button className={styles.searchButton} onClick={handleSearch}>Pesquisar</button>
                         </div>
-
                         <div className={styles.filterButtons}>
                             <div className={styles.filterGroup}>
                                 <label htmlFor="status" className={styles.labelFilter}>Status</label>
-                                <select id="status" className={styles.filterSelect}>
+                                <select 
+                                    id="status" 
+                                    className={styles.filterSelect} 
+                                    value={statusFilter} 
+                                    onChange={(e) => setStatusFilter(e.target.value)}
+                                >
                                     <option value="todos">Todos</option>
                                     <option value="ativo">Ativo</option>
                                     <option value="inativo">Inativo</option>
@@ -172,17 +134,21 @@ export default function CadCliente() {
 
                             <div className={styles.filterGroup}>
                                 <label htmlFor="tipoUsuario" className={styles.labelFilter}>Tipo de Usuário</label>
-                                <select id="tipoUsuario" className={styles.filterSelect}>
+                                <select 
+                                    id="tipoUsuario" 
+                                    className={styles.filterSelect} 
+                                    value={tipoUsuarioFilter} 
+                                    onChange={(e) => setTipoUsuarioFilter(e.target.value)}
+                                >
                                     <option value="todos">Todos</option>
                                     <option value="admin">Admin</option>
                                     <option value="usuario">Usuário</option>
                                 </select>
                             </div>
 
-                            <button className={styles.newButton}>Novo</button>
+                            <button className={styles.newButton} onClick={() => setShowForm(true)}>Novo</button>
                         </div>
                     </div>
-
 
                     <div className={styles.resultTableContainer}>
                         <table className={styles.resultTable}>
@@ -199,19 +165,19 @@ export default function CadCliente() {
                                 </tr>
                             </thead>
                             <tbody className={styles.tableBody}>
-                                {currentUsers.length > 0 ? (
-                                    currentUsers.map((usuario) => (
+                                {filteredUsers.length > 0 ? (
+                                    filteredUsers.map((usuario) => (
                                         <tr key={usuario.usu_id}>
                                             <td className={styles.tdId}>{usuario.usu_id}</td>
                                             <td>{usuario.usu_nome}</td>
                                             <td>{usuario.usu_cpf}</td>
                                             <td>{format(new Date(usuario.usu_data_nasc), 'dd/MM/yyyy')}</td>
-                                            <td>{sexoMap[usuario.usu_sexo] || 'Desconhecido'}</td>
+                                            <td>{usuario.usu_sexo}</td>
                                             <td>{usuario.usu_telefone}</td>
                                             <td>{usuario.usu_email}</td>
                                             <td>
                                                 <div className={styles.actionIcons}>
-                                                    <i> <MdRemoveRedEye title="Visualizar" onClick={() => handleViewUser(usuario)} /></i>
+                                                    <i><MdRemoveRedEye title="Visualizar" onClick={() => handleViewUser(usuario)} /></i>
                                                     <i><MdEdit title="Editar" onClick={() => handleEditUser(usuario)} /></i>
                                                     <i><IoMdTrash title="Excluir" /></i>
                                                 </div>
@@ -224,28 +190,10 @@ export default function CadCliente() {
                                     </tr>
                                 )}
                             </tbody>
-
                         </table>
                     </div>
-                    <div className={styles.pagination}>
-                        <button
-                            className={styles.buttonPrev}
-                            onClick={() => setCurrentPage(prev => Math.max(prev - 1, 1))}
-                            disabled={currentPage === 1}
-                        >
-                            Anterior
-                        </button>
-                        <span>Página {currentPage}</span>
-                        <button
-                            onClick={() => setCurrentPage(prev => (usuarios.length > indexOfLastUser ? prev + 1 : prev))}
-                            disabled={usuarios.length <= indexOfLastUser}
-                        >
-                            Próxima
-                        </button>
-                    </div>
                 </>
-            ) : (<>
-
+            ) : (
                 <FormCliente
                     selectedUser={selectedUser}
                     setSelectedUser={setSelectedUser}
@@ -253,14 +201,7 @@ export default function CadCliente() {
                     handleSubmit={handleSubmit}
                     Cancelar={Cancelar}
                 />
-
-                <div className={styles.footer_form}>
-                    <button type="reset" onClick={Cancelar} className={styles.button_cancel}>Cancelar</button>
-                    <button type="button" className={styles.button_submit} onClick={handleSubmit} disabled={isViewing}>Salvar</button>
-
-
-                </div>
-            </>)}
+            )}
         </div>
     );
 }
