2044b176c14390db045ff2c021c63522fb65f9d6
Nei Junio Nogueira Gomes <152228340+NeiJunio@users.noreply.github.com>
2024-10-03 20:36:17 -0300

    commit

--- src/components/relacionarVeiculo/index.jsx
+++ src/components/relacionarVeiculo/index.jsx
@@ -5,10 +5,9 @@ import Swal from 'sweetalert2';
 
 export default function ModalRelacionarVeiculo({ isOpen, onClose }) {
     const [nome, setNome] = useState('');
-    const [veiculos, setVeiculos] = useState([]); // Armazena os veículos encontrados
-    const [veiculoSelecionado, setVeiculoSelecionado] = useState(null); // Armazena o veículo selecionado
+    const [veiculos, setVeiculos] = useState([]);
+    const [veiculoSelecionado, setVeiculoSelecionado] = useState(null);
 
-    // Função para buscar veículos conforme o nome digitado
     const buscarVeiculos = async (nomeDigitado) => {
         if (nomeDigitado.trim()) {
             try {
@@ -18,28 +17,27 @@ export default function ModalRelacionarVeiculo({ isOpen, onClose }) {
                 console.error("Erro ao buscar veículos:", error);
             }
         } else {
-            setVeiculos([]); // Limpa a lista se o campo estiver vazio
+            setVeiculos([]);
         }
     };
 
-    // Efeito para buscar veículos sempre que o nome mudar
     useEffect(() => {
         buscarVeiculos(nome);
     }, [nome]);
 
     const handleSelectVeiculo = (veic_id) => {
-        setVeiculoSelecionado(veic_id); // Atualiza o veículo selecionado
+        setVeiculoSelecionado(veic_id);
     };
 
     const handleSalvar = async () => {
         if (veiculoSelecionado) {
             try {
-                const response = await api.patch(`/veiculo_usuario`, {
-                    veic_id: veiculoSelecionado,
+                await api.patch(`/veiculo_usuario`, {
+                    veic_ids: [veiculoSelecionado],
                     usu_id: 1 // Substituir pelo ID do usuário correto
                 });
                 Swal.fire('Sucesso', 'Veículo associado com sucesso!', 'success');
-                onClose(); // Fecha o modal após o sucesso
+                onClose();
             } catch (error) {
                 console.error("Erro ao associar veículo:", error);
                 Swal.fire('Erro!', 'Erro ao associar veículo.', 'error');
@@ -51,47 +49,55 @@ export default function ModalRelacionarVeiculo({ isOpen, onClose }) {
 
     return (
         <div className={styles.modalOverlay}>
-    <div className={styles.modalContent}>
-        <h2 className={styles.modalTitle}>Associar Veículo</h2>
-        <div className={styles.formGroup}>
-            <label htmlFor="nome">Nome do veículo</label>
-            <input
-                type="text"
-                id="nome"
-                value={nome}
-                onChange={(e) => setNome(e.target.value)}
-                required
-                className={styles.inputCategoria}
-            />
-            <ul className={styles.veiculosList}>
-                {veiculos.map((veiculo) => (
-                    <li key={veiculo.veic_id} className={styles.veiculoItem}>
-                        {veiculo.veic_id} - {veiculo.veic_placa} - {veiculo.mod_nome}
-                        <select
-                            onChange={() => handleSelectVeiculo(veiculo.veic_id)}
-                            className={styles.selectVeiculo}
-                            value={veiculoSelecionado === veiculo.veic_id ? veiculo.veic_id : ''}
-                        >
-                            <option value="">Selecionar</option>
-                            <option value={veiculo.veic_id}>Selecionar</option>
-                        </select>
-                    </li>
-                ))}
-            </ul>
+            <div className={styles.modalContent}>
+                <h2 className={styles.modalTitle}>Associar Veículo</h2>
+                <div className={styles.formGroup}>
+                    <label htmlFor="nome">Nome do veículo</label>
+                    <input
+                        type="text"
+                        id="nome"
+                        value={nome}
+                        onChange={(e) => setNome(e.target.value)}
+                        required
+                        className={styles.inputCategoria}
+                    />
+                    <ul className={styles.veiculosList}>
+                        <li className={styles.veiculosHeader}>
+                            <span className={styles.spanInput}></span> {/* Espaço para o input */}
+                            <span className={styles.spanId}>ID</span>
+                            <span>Placa</span>
+                            <span>Modelo</span>
+                        </li>
+                        {veiculos.map((veiculo) => (
+                            <li key={veiculo.veic_id} className={styles.veiculoItem}>
+                                <span>
+                                    <input
+                                        type="radio"
+                                        name="veiculo"
+                                        onChange={() => handleSelectVeiculo(veiculo.veic_id)}
+                                        checked={veiculoSelecionado === veiculo.veic_id}
+                                        className={styles.radioVeiculo}
+                                    />
+                                </span>
+                                <span className={styles.spanId}>{veiculo.veic_id}</span>
+                                <span>{veiculo.veic_placa}</span>
+                                <span>{veiculo.mod_nome}</span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className={styles.buttonGroup}>
+                    <button type="button" onClick={onClose} className={styles.btnCancel}>Cancelar</button>
+                    <button
+                        type="button"
+                        onClick={handleSalvar}
+                        className={styles.btnSave}
+                        disabled={!veiculoSelecionado}
+                    >
+                        Salvar
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className={styles.buttonGroup}>
-            <button 
-                type="button" 
-                onClick={handleSalvar} 
-                className={styles.btnSave} 
-                disabled={!veiculoSelecionado}
-            >
-                Salvar
-            </button>
-            <button type="button" onClick={onClose} className={styles.btnCancel}>Cancelar</button>
-        </div>
-    </div>
-</div>
-
     );
 }
